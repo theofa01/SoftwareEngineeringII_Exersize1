44227ff375038e33253e234eb92b8a3f4792518b
Thodoris <thodorisfa@gmail.com>
2026-03-22 22:59:50 +0200

    Implement equality, hash, and ToString for BookEdition
    BookEdition now implements IEquatable<BookEdition> and overrides Equals, GetHashCode, and ToString. Equality is based on key properties, and ToString provides a detailed string representation.

--- SE_Exersize1/BookEdition.cs
+++ SE_Exersize1/BookEdition.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SE_Exersize1
 {
-    public class BookEdition
+    public class BookEdition: IEquatable<BookEdition>
     {
         public string Isbn { get; }
         public string Title { get; }
@@ -21,6 +21,68 @@ namespace SE_Exersize1
             EditionNumber = editionNumber;
             PublicationYear = publicationYear;
             DecimalPrice = decimalPrice;
+        }
+
+        public bool Equals(BookEdition? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (this == other)
+            {
+                return true;
+            }
+
+            if (other.Isbn == this.Isbn && other.Title == this.Title && other.Author == this.Author
+                && other.EditionNumber == this.EditionNumber
+                && other.PublicationYear == this.PublicationYear)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not BookEdition)
+            {
+                return false;
+            }
+
+            BookEdition book = obj as BookEdition;
+
+            if (base.Equals(book))
+            {
+                return true;
+            }
+
+            if (book.Isbn == this.Isbn && book.Title == this.Title && book.Author == this.Author
+                && book.EditionNumber == this.EditionNumber 
+                && book.PublicationYear == this.PublicationYear)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Isbn.Length + this.Title.Length + this.Author.Length +
+                   this.EditionNumber + this.PublicationYear;
+        }
+
+        public override string ToString()
+        {
+            return $"Isbn: {this.Isbn}" +
+                   $"Title: {this.Title}" +
+                   $"Author: {this.Author}" +
+                   $"EditionNumber: {this.EditionNumber}" +
+                   $"PublicationYear: {this.PublicationYear}" +
+                   $"DecimalPrice: {this.DecimalPrice}";
         }
     }
 }
