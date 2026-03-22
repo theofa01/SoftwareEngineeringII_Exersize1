1147d94faef7e434334e5329f81ee7eaf2eb1e7a
Thodoris <thodorisfa@gmail.com>
2026-03-22 22:42:17 +0200

    Add project files.

---
+++ SE_Exersize1/BookEdition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Exersize1
+{
+    public class BookEdition
+    {
+        public string Isbn { get; }
+        public string Title { get; }
+        public string Author { get; }
+        public int EditionNumber { get; }
+        public int PublicationYear { get; }
+        public int DecimalPrice { get; }
+
+        public BookEdition(string isbn, string title, string author, int editionNumber, int publicationYear, int decimalPrice)
+        {
+            Isbn = isbn;
+            Title = title;
+            Author = author;
+            EditionNumber = editionNumber;
+            PublicationYear = publicationYear;
+            DecimalPrice = decimalPrice;
+        }
+    }
+}
---
+++ SE_Exersize1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SE_Exersize1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
