564c91e864cdeb376c5449001000d10918a1956f
Thodoris <thodorisfa@gmail.com>
2026-03-24 17:45:23 +0200

    Demonstrate custom comparers and equality in collections
    Added examples of sorting BookEdition objects using both default and custom comparers, and illustrated the effects of different equality comparers in HashSet and SortedSet collections. Enhanced output and comments to clarify the impact of these comparers on collection behavior.

--- SE_Exersize1/Program.cs
+++ SE_Exersize1/Program.cs
@@ -28,10 +28,36 @@
             }
 
             books.Sort();
-            Console.WriteLine("Books sorted by the IComparable interface: ");
+            Console.WriteLine("Books sorted by the IComparable interface (default comparer): ");
             Console.WriteLine("------------------------");
             PrintBooks(books);
             Console.WriteLine();
+
+            books.Sort(new BookEditionYearTitleComparer());
+            Console.WriteLine("Books sorted by the IComparer interface (year title): ");
+            Console.WriteLine("------------------------");
+            PrintBooks(books);
+            Console.WriteLine(); // this time we have different order because the
+                                 // IComparer interface sorts only by year and title
+            HashSet<BookEdition> bookHashSetDefault = new HashSet<BookEdition>(books);
+            HashSet<BookEdition> bookHashSetAlternative = new HashSet<BookEdition>(books, 
+                new BookEditionIsbnEqualityComparer());
+
+            Console.WriteLine("Books in the HashSet with the default equality class: ");
+            Console.WriteLine("------------------------");
+            PrintBooks(bookHashSetDefault.ToList());
+            Console.WriteLine();
+
+            Console.WriteLine("Books in the HashSet with the alternative equality class: ");
+            Console.WriteLine("------------------------");
+            PrintBooks(bookHashSetAlternative.ToList());
+            Console.WriteLine(); //this time the two different editions of the books Salem's Lot
+                                 //and The Hobbit do not appear
+                                 //twice in the Hashset as before because the
+                                 //alternative equality class compare only by the isbn
+            SortedSet<BookEdition> bookSortSetDefault = new SortedSet<BookEdition>(books);
+            SortedSet<BookEdition> bookSortSetAlternative = new SortedSet<BookEdition>(books,
+                new BookEditionYearTitleComparer());
         }
 
         private static void PrintBooks(List<BookEdition> books)
